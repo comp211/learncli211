2c0fec475730c26b940ef0137548cd9dde0526b5
Cindy Wang <cwindy@live.unc.edu>
2021-01-15 20:33:01 +0000

    lab 00 v0

---
+++ workdir/0-hello-world/hello.c
@@ -0,0 +1,11 @@
+// PID: 73033134
+// I pledge the COMP 211 honor code.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("hello, world\nwelcome to c!");
+
+    return EXIT_SUCCESS;
+}
