b595e7733481392e26b1e5e3964fd19e8b66a81a
Kris Jordan <kris@cs.unc.edu>
2024-08-19 15:18:00 -0400

    Update docker compose syntax in startup scripts

--- learncli.ps1
+++ learncli.ps1
@@ -4,5 +4,5 @@ param
     $command = "comp211"
 )
 
-Invoke-Expression "docker-compose pull $command"
-Invoke-Expression "docker-compose run --rm $command"
+Invoke-Expression "docker compose pull $command"
+Invoke-Expression "docker compose run --rm $command"
